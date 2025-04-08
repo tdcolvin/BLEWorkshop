b6fde684485e0bd113135a673fde4391a2a6afbc
Tom Colvin <tdcolvin@gmail.com>
2025-04-08 11:37:59 +0100

    Android SDK 35

--- app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             BLEClientTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.systemBarsPadding().fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                    MainNavigation()
