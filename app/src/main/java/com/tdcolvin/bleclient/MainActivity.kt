83e0f2e7d711b3236e1a338c2068acebc4fb197d
Tom Colvin <tdcolvin@gmail.com>
2025-04-08 22:00:19 +0100

    Fixed theme colours and edge-to-edgeness

--- app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
@@ -2,7 +2,9 @@ package com.tdcolvin.bleclient
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
@@ -14,15 +16,18 @@ import com.tdcolvin.bleclient.ui.theme.BLEClientTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             BLEClientTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.systemBarsPadding().fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                    MainNavigation(
-                       modifier = Modifier.fillMaxSize()
+                       modifier = Modifier.systemBarsPadding().fillMaxSize()
                    )
                 }
             }
