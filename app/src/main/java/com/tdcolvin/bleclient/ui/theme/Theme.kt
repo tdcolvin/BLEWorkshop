fb56f025eb9d7dce31e97b3cc1597a9e7cade5e1
Tom Colvin <tdcolvin@gmail.com>
2025-04-08 22:00:19 +0100

    Fixed theme colours and edge-to-edgeness

--- app/src/main/java/com/tdcolvin/bleclient/ui/theme/Theme.kt
+++ app/src/main/java/com/tdcolvin/bleclient/ui/theme/Theme.kt
@@ -57,8 +57,7 @@ fun BLEClientTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
